c0e9ac391a6355e4f6a90a770442b38e460499ee
Juan Zaragoza <56614909+ItsJuanito@users.noreply.github.com>
2020-08-16 12:05:42 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
 const api = {
     key: "a124aa17ed99d3ec1b1446055259df82",
     base: "https://api.openweathermap.org/data/2.5/"
-}
+} // api key and base
+
+let mainTemp, min, max, newMain, newMax, newMin;
+// set global variables to use for onclick event
 
 const searchBox = document.querySelector('.search-box');
 searchBox.addEventListener('keypress', setQuery);
+
+let buttonF = document.querySelector(".F");
+let buttonC = document.querySelector(".C");
+
+buttonF.onclick = () => {
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(newMain)} <span>°F</span>`;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(newMin)}°F / ${Math.round(newMax)}°F`;
+} // converts the degrees to Fahrenheit
+
+buttonC.onclick = () => {
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(mainTemp)} <span>°C</span>`;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(min)}°C / ${Math.round(max)}°C`;
+} // converts the degree to Celsuis
 
 function setQuery(evt) {
     if (evt.keyCode == 13) {
         getResults(searchBox.value);
         console.log(searchBox.value);
-    } //presses the enter key
+    } //presses the enter key then calls getResult function
 }
 
 function getResults(query) {
@@ -18,7 +40,7 @@ function getResults(query) {
     .then(weather => {
         return weather.json();
     }).then(displayResults);
-} // fetches api and gets results and query
+} // fetches api and gets results and query then displayResults is executed
 
 function displayResults(weather) {
     console.log(weather);
@@ -28,34 +50,22 @@ function displayResults(weather) {
     date.innerText = dateBuilder();
 
     let temp = document.querySelector('.current .temp');
-    let newMain = (weather.main.temp) * 1.8 + 32;
-    temp.innerHTML = `${Math.round(newMain)} <span>°F</span>`
+    mainTemp = weather.main.temp;
+    newMain = (mainTemp) * 1.8 + 32;
+    temp.innerHTML = `${Math.round(newMain)} <span>°F</span>`;
 
     let weather_cast = document.querySelector('.current .weather');
     let cast = weather.weather[0].main;
     weather_cast.innerText = cast;
 
     let hilow = document.querySelector('.hi-low');
-    let min = (weather.main.temp_min) * 1.8 + 32;
-    let max = (weather.main.temp_max) * 1.8 + 32;
-    hilow.innerText = `${Math.round(min)}°F / ${Math.round(max)}°F`;
+    min = weather.main.temp_min;
+    max = weather.main.temp_max;
+    newMin = (min) * 1.8 + 32;
+    newMax = (max) * 1.8 + 32;
+    hilow.innerText = `${Math.round(newMin)}°F / ${Math.round(newMax)}°F`;
     checkCast(cast);
-
-    //the commented code displays the temperature in Celsuis
-    /*let temp = document.querySelector('.current .temp');
-    temp.innerHTML = `${Math.round(weather.main.temp)} <span>°C</span>`
-
-    let weather_cast = document.querySelector('.current .weather');
-    let cast = weather.weather[0].main;
-    weather_cast.innerText = cast;
-
-    let hilow = document.querySelector('.hi-low');
-    let min = Math.round(weather.main.temp_min);
-    let max = Math.round(weather.main.temp_max);
-    hilow.innerText = `${min}°C / ${max}°C`;
-    checkCast(cast);*/
-    
-}
+} // displays all query from api to index.html
 
 function dateBuilder() {
     let months = ["January", "February", "March", "April", "May", 
@@ -72,7 +82,7 @@ function dateBuilder() {
     let year = d.getFullYear();
 
     return `${day} ${month} ${date}, ${year}`;
-}
+} // gets day, month, date, and year (of search)
 
 function checkCast(c) {
     if (c == "Sunny" || c == "Clear") {
@@ -82,4 +92,4 @@ function checkCast(c) {
     } else if (c == "Rain") {
         document.body.style.backgroundImage = "url('./img/rainy.jpg')";
     }
-}
+} // changes background image bases on the forecast
